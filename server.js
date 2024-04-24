b46a1e927d113256bd91be2913b3c57f50a3aae6
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2024-04-24 18:12:03 +0530

    Add python process

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+const { spawn } = require('child_process');
 const http = require('http');
+const socketIO = require('socket.io');
 const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -13,6 +16,12 @@ app.get('/', (req, res) => {
     res.status(200).sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.post('/generate', (req, res) => {
+    const { initialSeed, temperature, steps } = req.body;
+
+    const pythonProcess = spawn('python', ['generator.py', initialSeed, temperature, steps]);
+
+});
 
 server.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
